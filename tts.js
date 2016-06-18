eb2e11507662e1965ed3354317adfe44f6dc516d
Ian Forrester <cubicgarden@users.noreply.github.com>
2016-06-18 13:58:55 +0100

    Update tts.js

--- tts.js
+++ tts.js
@@ -1,1 +1,5 @@
-// paste code here
+function talk(speech){
+	var msg = new SpeechSynthesisUtterance(speech);
+msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == 'Google UK English Male'; })[0];
+speechSynthesis.speak(msg);
+	}
